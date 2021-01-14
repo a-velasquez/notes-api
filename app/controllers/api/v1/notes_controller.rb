9812056d60675aed0fcc820a8f8fd750c73d7e7d
Alex Velasquez <avelasquez075@gmail.com>
2021-01-14 15:42:18 -0800

    add destroy action

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -23,6 +23,13 @@ class Api::V1::NotesController < ApplicationController
     @note.update(note_params)
     render json: @note, status: 200 
   end
+  
+  def destroy 
+    @note = Note.find(param[:id])
+    @note.delete 
+
+    render json: {noteId: @note.id}
+  end
 
   private 
     def notes_params
