9e41b8aede6de07b27ea93a2bcce59d7a4f589a9
a-velasquez <avelasquez075@gmail.com>
2021-01-15 01:26:12 -0800

    add notes to NotesAdapter

--- js-frontend/src/adapters/NotesAdapter.js
+++ js-frontend/src/adapters/NotesAdapter.js
@@ -1,3 +1,7 @@
+/*
+Sole purpose of adapter is to connect to api
+sometimes called service or adapter
+*/
 class NotesAdapter {
   constructor() {
     this.baseUrl = "http://localhost:3000/api/v1/notes"
