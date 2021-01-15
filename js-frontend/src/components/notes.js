f870ca76a7011cb9d1ad241d9df6d98f7ae0d272
a-velasquez <avelasquez075@gmail.com>
2021-01-15 00:36:37 -0800

    add fetchAndLoadNotes method

--- js-frontend/src/components/notes.js
+++ js-frontend/src/components/notes.js
@@ -2,6 +2,13 @@ class Notes {
   constructor() {
     this.notes = []
     this.adapter = new NotesAdapter()
-    this.bindEventListeners()
+    // this.bindEventListeners()
+    this.fetchAndLoadNotes()
+  }
+
+  fetchAndLoadNotes() {
+    this.adapter.getNotes().then(notes => {
+      console.log(notes)
+    })
   }
 }
