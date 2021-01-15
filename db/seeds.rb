08f5e16244484a4dc9d193c60b9266188cd8310b
Alex Velasquez <avelasquez075@gmail.com>
2021-01-14 17:40:19 -0800

    add notes model, schema, and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Notes.create([
+  { body: 'Plan what features your app will have. You can write User Stories to help make it clear what you are planning to build'},
+  { body: 'Model your domain. You need to know what the nouns in your project are - the objects in the 'world' of your application. It can be helpful to draw the relationships between your models'},
+  { body: 'Plan how your features will work.'},
+  { body: 'A user is able to view travel locations, add new travel locations, edit travel locations and delete travel locations.'},
+  { body: 'A user is able to review a travel location'},
+  { body: 'As you turn your user stories into more clear technical specifications for features, you can start by modeling the data that your application will store and show. Identify the nouns in your stories, their properties, and their relationships.'},
+  { body: 'A review has a comment and a rating'},
+  { body: 'A travel location has a name, a description, a location.'},
+  { body: 'Planning: Write down your ideas (use diagrams!)'}
+])
