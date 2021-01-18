e31ab86065994bf784c53cdb1b4bdc1324190091
a-velasquez <avelasquez075@gmail.com>
2021-01-18 00:40:50 -0800

    add initBindings method, sort to fetch method and rewrite render method

--- js-frontend/src/components/notes.js
+++ js-frontend/src/components/notes.js
@@ -2,24 +2,30 @@ class Notes {
   constructor() {
     this.notes = [];
     this.adapter = new NotesAdapter();
-    // this.initiBindingsAndEventListeners();
+    this.initiBindingsAndEventListeners();
     this.fetchAndLoadNotes();
+  }
+
+  initiBindingsAndEventListeners() {
+    this.notesContainer = document.getElementById("notes-container");
   }
 
   fetchAndLoadNotes() {
     this.adapter
-    .getNotes()
-    .then(notes => {
-      notes.forEach(note => this.notes.push(note))
-    })
-    .then(() => {
-      this.render()
-    })
+      .getNotes()
+      .then((notes) => {
+        notes
+          .sort((a, b) => a.id - b.id)
+          .forEach((note) => this.notes.push(new Note(note)));
+      })
+      .then(() => {
+        this.render();
+      });
   }
 
   render() {
-    const notesContainer = document.getElementById("notes-container")
-    notesContainer.innerHTML = 'my notes here'
-    console.log('My notes are', this.notes)
+    this.notesContainer.innerHTML = this.notes
+      .map((note) => note.renderLi())
+      .join("");
   }
 }
